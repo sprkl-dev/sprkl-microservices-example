02f99a2e2e89ec7189972ffb33e3fd28abcc8999
Noam Wertheim <noam.wertheim@gmail.com>
2022-07-27 14:50:12 +0300

    added unit tests

---
+++ catalog/catalog.test.js
@@ -0,0 +1,13 @@
+const getCatalog = require('./catalog')
+
+const redis = require('redis-mock')
+
+jest.mock('redis', () => jest.requireActual('redis-mock'));
+
+test('whatever', async () => {
+    const catalog = await getCatalog()
+    expect(catalog[0].id).toBe("1")
+    expect(catalog[0].name).toBe(`t-shirt`)
+    expect(catalog[0].price).toBe(300)
+})
+
--- orders/index.js
+++ orders/index.js
@@ -2,6 +2,7 @@ const fastify = require('fastify')({ logger: { level: 'trace' } })
 const crypto = require('crypto')
 const {MongoClient} = require('mongodb');
 const {default: axios} = require('axios');
+const {getTotalPrice, pay} = require("./orders");
 
 const mongoHost = process.env.MONGO_HOST
 const mongoPort = process.env.MONGO_PORT ?? 27017
@@ -23,44 +24,6 @@ async function bootstap() {
   }
 }
 
-async function pay({amount, orderId}) {
-  console.log({amount, orderId})
-  try {
-    await axios.post(`${paymentsURL}/payments`, { amount, orderId })
-    return true
-  } catch(e) {
-    fastify.log.error(e)
-    return false
-  }
-}
-
-async function getTotalPrice(itemNames) {
-  try {
-    const catalog = await (await axios.get(`${catalogURL}/catalog`)).data
-    console.log(catalog);
-    const itemPriceByName = new Map();
-    const itemPriceById = new Map();
-    for (const item of catalog) {
-      itemPriceByName.set(item.name, item.price)
-      itemPriceById.set(item.id, item.price)
-    }
-    let totalPrice = 0;
-    for (const item of itemNames) {
-      if (itemPriceByName.has(item)) {
-        totalPrice += itemPriceByName.get(item)
-      } else if (itemPriceById.has(item)){
-        totalPrice += itemPriceById.get(item)
-      } else {
-        fastify.log.error({item: item, msg: 'failed to find an item'})
-        return -1;
-      }
-    }
-    return totalPrice
-  } catch(e) {
-    fastify.log.error(e)
-    return -1;
-  }
-}
 
 fastify.post('/orders', async function (request, reply) {
   const { items } = request.body;

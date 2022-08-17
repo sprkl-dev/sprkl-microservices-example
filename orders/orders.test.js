df9b82668788d9eb7c022422537fde04781666d3
Erez Voitiz <erez@sprkl.dev>
2022-08-17 11:34:10 +0300

    not working

--- orders/orders.test.js
+++ orders/orders.test.js
@@ -6,7 +6,6 @@ jest.mock('axios')
 
 test('should make pay', async () => {
     axios.post.mockResolvedValue({data: []})
-
     const paid = await pay({amount: "totalPrice", orderId: "whatever"})
     expect(paid).toBe(true)
 })

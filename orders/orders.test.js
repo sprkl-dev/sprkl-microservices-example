5550421d37acf77f0f454d3c7fa88ed99a0082c2
Erez Voitiz <erez@sprkl.dev>
2022-08-18 13:11:23 +0300

    improved testing

--- orders/orders.test.js
+++ orders/orders.test.js
@@ -1,10 +1,14 @@
 jest.mock('axios')
 const axios = require('axios')
 
-const {pay} = require('./orders')
-test('should make pay', async () => {
-    axios.post.mockResolvedValue({data: []})
+const { pay } = require('./orders')
 
-    const paid = await pay({amount: "totalPrice", orderId: "whatever"})
-    expect(paid).toBe(true)
+describe('Orders test suite', () => {
+
+    test('should make pay', async () => {
+        axios.post.mockResolvedValue({ data: [] })
+
+        const paid = await pay({ amount: "totalPrice", orderId: "whatever" })
+        expect(paid).toBe(true)
+    })
 })
